3d0f3f12490644bf0987166e9099a8b3ae9cf30d
Prithvi <prithvisingh91827@gmail.com>
2022-06-16 01:31:53 +0530

    I am adding subsequence recursion file code

---
+++ subsequence_recursion.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void subS(int ind, vector<int> &v, int arr[], int n){
+    if(ind==n){
+        for(auto it:v){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+        return;
+    }
+    v.push_back(arr[ind]);
+    subS(ind+1,v,arr,n);
+    v.pop_back();
+
+    subS(ind+1,v,arr,n);
+}
+
+int main(){
+    int arr[] = {1,3,2};
+    vector<int> v;
+    subS(0,v,arr,3);
+    cout<<"p"<<endl;
+}
